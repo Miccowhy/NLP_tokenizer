3e9cdfdc0a5f1e7d0020c62e94bb59a435dc1ca0
Miccowhy <mikale1@st.amu.edu.pl>
2019-11-18 02:14:19 +0100

    En dash, punctuation

--- main.py
+++ main.py
@@ -17,35 +17,57 @@ def parse_cli():
 
     cli_group.add_argument("-f", "--file",
                            help="path to the input file")
-    return cli_parser.parse_args()
+    return cli_parser.parse_args(), cli_parser
 
 
 def tokenize(data):
-    splitted = space_splitter(data)
-    print(splitted)
-    #geo_data = geo_names(splitted)
+    split = space_splitter(data)
+    print(f"Split: {split}")
 
     undotted = list()
-    for word in splitted:
-        if isinstance(word, tuple):
-            [undotted.append(sub) for sub in dot_remover(word)]
+    #for word in splitted:
+    for word in split:
+        checked = dot_remover(word)
+        if isinstance(checked, tuple):
+            [undotted.append(subword) for subword in checked]
         else:
             undotted.append(dot_remover(word))
-    print(undotted)
+    #print(f"Undotted: {undotted}")
 
-    tokenized_data = list()
+    decontractioned = list()
     for word in undotted:
-        if isinstance(word, tuple):
-            [tokenized_data.append(sub) for sub in contraction_check(word)]
+        checked = contraction_check(word)
+        if isinstance(checked, tuple):
+            [decontractioned.append(subword) for subword in checked]
         else:
-            tokenized_data.append(contraction_check(word))
-    print(tokenized_data)
+            decontractioned.append(checked)
+    #print(f"Deco: {decontractioned}")
+
+    punctuation = list()
+    for word in decontractioned:
+        checked = punctuation_remover(word)
+        if isinstance(checked, tuple):
+            [punctuation.append(subword) for subword in checked]
+        else:
+            punctuation.append(checked)
+    #print(f"Punc: {punctuation}")
+
+    undashed = list()
+    for word in punctuation:
+        checked = en_dash_remover(word)
+        if isinstance(checked, tuple):
+            [undashed.append(subword) for subword in checked]
+        else:
+            undashed.append(checked)
+    #print(f"Undashed: {undashed}")
+
+    tokenized_data = geo_names(undashed)
 
     return tokenized_data
 
 
 if __name__ == "__main__":
-    args = parse_cli()
+    args, helper = parse_cli()
     data = None
 
     if args.interactive:
@@ -54,6 +76,7 @@ if __name__ == "__main__":
         with open(args.file, 'r') as input_file:
             data = input_file
     else:
+        #helper.print_help()
         data = input()
 
     if data is not None:
--- tokenizer.py
+++ tokenizer.py
@@ -5,6 +5,8 @@ dot_regex = re.compile("[a-z]{2,4}\.|"
                        "[A-Z][a-z]{0,3}\.|"
                        "([A-Za-z]\.){2,}")
 
+punctuation_regex = re.compile("\w+([\-,\[\]:;\"?!]+|\.\.\.)$")
+
 contraction_regex = re.compile("\w+("
                                "'ll|"
                                "n't|"
@@ -19,7 +21,7 @@ en_dash_regex = re.compile("(\d+|"
                             "((January|February|March|April|May|June|July|August|September|October|November|December)"
                            "|\d+)")
 
-geo_list = pandas.read_csv("world-cities.csv", usecols=['name'])
+geo_list = pandas.read_csv("world-cities.csv")['name'].values.tolist()
 
 
 def space_splitter(data):
@@ -36,27 +38,42 @@ def dot_remover(word):
         return word
 
 
+def punctuation_remover(word):
+    punctuation = punctuation_regex.findall(word)
+    if punctuation:
+        return word.replace(punctuation[0], ""), punctuation[0]
+    else:
+        return word
+
+
+def en_dash_remover(word):
+    dash = en_dash_regex.match(word)
+    if dash is not None:
+        split = word.split("-")
+        return split[0], "-", split[1]
+    else:
+        return word
+
+
 def geo_names(data):
     for word in range(len(data)):
         try:
             if f"{data[word]} {data[word+1]}" in geo_list:
                 data[word] = f"{data[word]} {data[word+1]}"
-                data[word + 1].pop()
+                data.pop(word + 1)
             elif f"{data[word]} {data[word+1]} {data[word+2]}" in geo_list:
                 data[word] = f"{data[word]} {data[word + 1]}"
-                data[word + 1].pop()
-                data[word + 2].pop()
+                data.pop(word + 1)
+                data.pop(word + 2)
         except IndexError:
             pass
+    return data
 
 
 def contraction_check(word):
     contraction = contraction_regex.findall(word)
     if contraction:
-        word = word.replace(contraction, '')
-        return word, contraction
+        word = word.replace(contraction[0], '')
+        return word, contraction[0]
     else:
         return word
-
-
-#if __name__ == "__main__":
